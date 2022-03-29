afc8e12f3d858a4f9109eacc6370a1c3bbc626be
hateVtubers <gmolloflores@gmail.com>
2022-03-29 18:42:05 -0300

    change: double quotes to single quotes

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,7 +1,7 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "https://api.ihateani.me/v2/graphql",
+  uri: 'https://api.ihateani.me/v2/graphql',
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
   reactStrictMode: true,
   images: {
-    domains: ["i.nhentai.net", "t.nhentai.net", "api.ihateani.me"],
+    domains: ['i.nhentai.net', 't.nhentai.net', 'api.ihateani.me'],
   },
   pwa: {
-    dest: "public",
+    dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
+    disable: process.env.NODE_ENV === 'development',
   },
 });
