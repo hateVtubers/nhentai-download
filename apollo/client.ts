eeeffaf79de6e0e45725fde3ae6a3be15e139a89
hateVtubers <gmolloflores@gmail.com>
2022-04-21 18:55:23 -0400

    remove: InMemoryCache

--- apollo/client.ts
+++ apollo/client.ts
@@ -2,17 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://api.ihateani.me/v2/graphql',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          nhentai: {
-            merge: true,
-          },
-        },
-      },
-    },
-  }),
+  cache: new InMemoryCache(),
 });
 
 export default client;
