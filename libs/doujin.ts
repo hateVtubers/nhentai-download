7097e442cedbe558592a28ec7469e436d320425c
hateVtubers <hatevtubers@gmail.com>
2022-06-22 16:54:26 -0400

    formating

--- libs/doujin.ts
+++ libs/doujin.ts
@@ -1,41 +1,39 @@
-import client from 'apollo/client';
-import { getDoujinFromUrl } from 'apollo/querys';
-import { storage } from 'libs/firebase/client';
-import { ref, uploadBytes } from 'firebase/storage';
+import client from 'apollo/client'
+import { getDoujinFromId } from 'apollo/querys'
+import { storage } from 'libs/firebase/client'
+import { ref, uploadBytes } from 'firebase/storage'
 
 export type Data = {
   title: {
-    english?: string;
-    japanese?: string;
-  };
+    english?: string
+    japanese?: string
+  }
   images: {
-    url: string;
-  }[];
-  url: string;
-  id: string;
-};
+    url: string
+  }[]
+  url: string
+  id: string
+}
 
 export const getDoujins = async (id: string[]) => {
   const datas: Data[] | null = await Promise.all(
     id.map((code) =>
       client
         .query({
-          query: getDoujinFromUrl,
+          query: getDoujinFromId,
           variables: {
             doujinId: code,
           },
         })
         .then(({ data }) => data.nhentai.info)
     )
-  );
+  )
 
-  return datas;
-};
+  return datas
+}
 
 export const submitToFirebaseStorage = async (blob: Blob, title: string) => {
-  const storageRef = ref(storage, title);
+  const storageRef = ref(storage, title)
 
-  await uploadBytes(storageRef, blob);
-
-  return blob;
-};
+  await uploadBytes(storageRef, blob)
+}
